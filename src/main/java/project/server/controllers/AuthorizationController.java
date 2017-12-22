0968c2317112d26cd6c3dd9a74cd88b3a84efc03
zonder129 <havofavo@mail.ru>
2017-12-22 19:28:18 +0300

    url update for production

--- src/main/java/project/server/controllers/AuthorizationController.java
+++ src/main/java/project/server/controllers/AuthorizationController.java
@@ -14,14 +14,13 @@ import java.util.Objects;
 
 @SuppressWarnings("unused")
 @RestController
-@CrossOrigin(origins = AuthorizationController.FRONTED_URL1)
+@CrossOrigin(origins = AuthorizationController.FRONTED_URL)
 public class AuthorizationController {
     private final UserService userService;
     private final PasswordEncoder encoder;
     private final ScoreboardService scoreboardService;
     @SuppressWarnings("WeakerAccess")
-    static final String FRONTED_URL1 = "https://lands-dangeous.herokuapp.com";
-    static final String FRONTED_URL2 = "https://dev-lands-dungeons.herokuapp.com";
+    static final String FRONTED_URL = "https://landsanddungeons.ru.com/";
 
 
     public AuthorizationController(UserService userService, PasswordEncoder encoder, ScoreboardService scoreboardService) {
